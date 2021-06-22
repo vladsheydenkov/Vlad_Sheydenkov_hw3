826bdbde3d52fb69ce267ae00f14b1c1684c7df2
vladshe <vladsheydenkv@gmail.com>
2021-06-22 19:12:46 +0300

    restructure homework directory

--- scenarios/conftest.py
+++ scenarios/conftest.py
@@ -7,9 +7,21 @@ import pytest
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium import webdriver
 
+from selenium import webdriver
+
+capabilities = {
+    "browserName": "chrome",
+    "browserVersion": "90.0",
+    "selenoid:options": {
+        "enableVNC": True,
+        "enableVideo": False
+    }
+}
+
+
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    driver = webdriver.Remote(command_executor="http://0.0.0.0:4444/wd/hub", desired_capabilities=capabilities)
     yield driver
     driver.quit()
--- scenarios/dynamic_table_page.py
+++ scenarios/dynamic_table_page.py
@@ -12,6 +12,12 @@ class SearchLocators():
     CPU = (By.CLASS_NAME, 'bg-warning')
 
 
+def find_cpu(list_with_elements):
+    for element in list_with_elements[1:]:
+        if '%' in element:
+            return f'Chrome CPU: {element}'
+
+
 class DynamicTableHelper(BasePage):
 
     def find_chrome_cpu(self):
@@ -19,8 +25,7 @@ class DynamicTableHelper(BasePage):
         for row in rows:
             data_from_row = row.text.split()
             if data_from_row[0] == 'Chrome':
-                chrome_cpu = data_from_row[1]
-                return f'Chrome CPU: {chrome_cpu}'
+                return find_cpu(data_from_row)
 
     def find_answer_cpu(self):
         return self.find_element(SearchLocators.CPU, time=2).text
